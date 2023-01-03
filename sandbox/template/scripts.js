19081defe06ad5941cc935fbfb5c592a1d1192d7
Joseph Muller <j.muller@bbk.ac.uk>
2023-01-03 19:00:33 +0000

    Add sandbox template

---
+++ sandbox/template/scripts.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+    /* Start of your code */
+
+    let string3 = 'the third string';
+`the next string is ${string3}`;
+console.log(`the next string is ${string3}`); 
+
+    /* End of your code */
+  })();  
