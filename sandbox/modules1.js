364cf15de0c84ab9c58458e0a112deee88266f49
Joseph Muller <j.muller@bbk.ac.uk>
2023-01-22 21:19:43 +0000

    Add ES modules sandbox

---
+++ sandbox/modules1.js
@@ -0,0 +1,8 @@
+import ordinal from "ordinal";
+
+export default {
+	whichNumber: 333,
+	ordinal333: function() {
+		return ordinal(333);
+	}
+};
---
+++ sandbox/modules2.js
@@ -0,0 +1,6 @@
+import modules from "./modules1.js";
+
+console.log(modules.whichNumber);
+// 333
+console.log(modules.ordinal333());
+// 333rd
