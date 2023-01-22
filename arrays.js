01b0dbc48ba00f32e1e3be29f68e775be33099b1
Joseph Muller <j.muller@bbk.ac.uk>
2023-01-22 13:05:23 +0000

    Draft arrays.js

---
+++ arrays.js
@@ -0,0 +1,54 @@
+const theSeasons = ['Spring', 'Summer', 'Autumn', 'Winter'];
+
+console.log(theSeasons);
+// ['Spring', 'Summer', 'Autumn', 'Winter']
+
+console.log(theSeasons[0]);
+// Spring
+console.log(theSeasons[1]);
+// Summer
+console.log(theSeasons[2]);
+// Autumn
+console.log(theSeasons[3]);
+// Winter
+
+console.log(theSeasons[10-8]);
+// Autumn
+console.log(theSeasons[1+2]);
+// Winter
+
+console.log(theSeasons.indexOf('Summer'));
+// 1
+console.log(theSeasons.indexOf('Winter'));
+// 3
+
+console.log(theSeasons[theSeasons.indexOf('Spring') + 1]);
+// Summer
+
+let changingSeasons = [theSeasons[0], theSeasons[2]];
+console.log(changingSeasons);
+// ['Spring', 'Autumn']
+
+let warmerSeasons = theSeasons.slice(0, 2);
+console.log(warmerSeasons);
+// ['Spring', 'Summer']
+
+let colderSeasons = theSeasons.slice(2);
+console.log(colderSeasons);
+// ['Autumn', 'Winter']
+
+let colderThenWarmerSeasons = colderSeasons.concat(warmerSeasons);
+console.log(colderThenWarmerSeasons);
+// ['Autumn', 'Winter', 'Spring', 'Summer']
+
+console.log(theSeasons.length);
+// 4
+console.log(theSeasons[3].length);
+// 6
+
+theSeasons.push('Spring on Mars');
+console.log(theSeasons);
+// ['Spring', 'Summer', 'Autumn', 'Winter', 'Spring on Mars']
+theSeasons.pop();
+console.log(theSeasons);
+// ['Spring', 'Summer', 'Autumn', 'Winter']
