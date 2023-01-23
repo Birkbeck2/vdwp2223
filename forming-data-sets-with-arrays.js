dfb439dd481c982c7b08d9014e081b2d1a11eafd
Joseph Muller <j.muller@bbk.ac.uk>
2023-01-23 17:51:16 +0000

    Test of permalink method

---
+++ forming-data-sets-with-arrays.js
@@ -0,0 +1,4 @@
+let foodsILike = ['vegy sosaJes', 'edamamay', 'sushi', 'seaweed',
+'sweets', 'licafish', 'fish fingers', 'chips', 'ice cream', 'mufans',
+'cookies', 'cupcake', 'cake', 'milk', 'tomatos', 'potatos',
+'karott', 'spagety', 'lasany'];
