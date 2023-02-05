3d6375b0a689fd50dbd52579c45c9fd91bedae4b
Joseph Muller <j.muller@bbk.ac.uk>
2023-02-05 19:44:26 +0000

    Events JS file

---
+++ events.js
@@ -0,0 +1,89 @@
+let q2 = document.querySelector('#q2');
+q2.remove();
+
+let a2 = document.getElementById('a2');
+a2.remove();
+
+
+
+
+
+let qa4 = document.createElement('div');
+let q4 = document.createElement('h2');
+let a4 = document.createElement('p');
+qa4.append(q4, a4);
+let greetingDiv = document.getElementById('greetings');
+greetingDiv.append(qa4);
+
+
+
+
+
+qa4.id = 'qa4';
+qa4.className = 'question-answer';
+q4.id = 'q4';
+q4.className = 'question greeting';
+q4.textContent = 'Hello again';
+a4.id = 'a4';
+a4.className = 'answer';
+a4.textContent = 'hello';
+
+
+
+
+
+let a1 = document.getElementById('a1');
+a1.innerHTML= '';
+
+
+
+
+
+let a3 = document.getElementById('a3');
+a3.innerHTML = '<em>Extremely</em> well, <span style="color: purple; font-size: 36px; text-shadow: #BC0 5px 0 10px;">thanks</span>';
+
+
+
+
+const switchButton = document.getElementById('switch');
+switchButton.addEventListener('click', changePicture);
+
+function changePicture() {
+	const picturePaths = [
+		'images/veg1.jpg',
+		'images/veg2.jpg'
+	]
+	img = document.getElementById('switching-picture');
+	console.log('Old path was', img.src);
+	if (img.src.includes(picturePaths[0])) {
+		img.src = picturePaths[1];
+	} else {
+		img.src = picturePaths[0];
+	}
+	console.log('New path is', img.src);
+}
+
+
+
+
+const greetings = document.getElementsByClassName('greeting');
+
+for (let greeting of greetings) {
+  greeting.addEventListener('click', sayAgain)
+}
+
+function sayAgain(event) {
+	event.target.innerHTML = event.target.innerHTML + ' ' + event.target.innerHTML;
+}
+
+
+
+
+const answers = document.getElementsByClassName('answer');
+
+for (let answer of answers) {
+	answer.addEventListener('click', event => {
+		event.target.innerHTML = event.target.innerHTML + ' ' + event.target.innerHTML;
+	});
+}
+
